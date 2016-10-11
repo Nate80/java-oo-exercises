cccab9c9c285d439ed6d3bc9bdd9b3fbd659ed8d
Nathan Mohr <nlm9380@gmail.com>
2016-10-10 19:57:20 -0500

    This should be it

---
+++ java-track/assignments/Course.java
@@ -0,0 +1,87 @@
+
+public class Course {
+	private String name;
+	private int credits;
+	private int remainingSeats;
+	private Student[] roster;
+	
+	public Course(String name, int credits, int seats) {
+		this.name = name;
+		this.credits = credits;
+		this.remainingSeats = seats;
+		this.roster = new Student[seats];
+	}
+	
+	//getters and setters
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public int getCredits() {
+		return this.credits;
+	}
+	
+	public int getRemainingSeats() {
+		return this.remainingSeats;
+		
+	}
+	
+	public Student[] getRoster() {
+		return this.roster;
+	}
+	
+	public boolean studentEnrolled(Student student) {
+		for(Student enrolled : this.roster) {
+			if(enrolled != null) {
+				if(enrolled.getName() == student.getName()) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	public boolean addStudent(Student student) {
+		if(this.remainingSeats == 0) {
+			return false;
+			} else {
+				for(Integer i = 0; i < this.roster.length; i = i + 1) {
+					if(this.roster[i] == null) {
+						this.roster[i] = student;
+						this.remainingSeats = this.remainingSeats - 1;
+						return true;
+					}
+				}
+			}
+		return false;
+	}
+	
+	public void generateRoster() {
+		System.out.println("Roster for " + this.name + ":");
+		for(Student student : this.roster) {
+			if(student == null) {
+				System.out.println(" Empty");
+			} else {
+				System.out.println(" " + student.getName());
+			}
+		}
+	}
+	
+	public double averageGPA() {
+		double totalGPA = 0;
+		for(Student student : this.roster) {
+			if(student != null) {
+				totalGPA = totalGPA + student.getGPA();
+			} else {
+				totalGPA = totalGPA;
+			}
+		}
+		double averageGPA = (totalGPA / this.roster.length - this.remainingSeats);
+		return averageGPA;
+	}
+	
+	public String toString() {
+		return("Course Number: " + this.name + "; Course Credits: " + this.credits);
+	}
+}
