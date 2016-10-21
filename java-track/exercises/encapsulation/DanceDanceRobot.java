9855e3cc64de2ea9971b6eaf75a0c28335d53bc4
Nathan Mohr <nlm9380@gmail.com>
2016-10-20 19:50:24 -0500

    Added new Dance Dance Robot

--- java-track/exercises/encapsulation/DanceDanceRobot.java
+++ java-track/exercises/encapsulation/DanceDanceRobot.java
@@ -1,5 +1,17 @@
 package encapsulation;
 
-public class DanceDanceRobot {
+public class DanceDanceRobot extends Robot {
 
+	private String direction;
+	
+	public DanceDanceRobot(String name, int x, int y, int speed, String orientation, String direction) {
+		super(name, x, y, speed, orientation);
+		this.direction = direction;
+	}
+	
+	public void danceDirection() {
+		if(this.orientation = "North") {
+				
+		}
+	}
 }
