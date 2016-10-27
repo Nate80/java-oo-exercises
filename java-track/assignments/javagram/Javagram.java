ca534ee5c614e70633cc133135bed45026e4276f
Nathan Mohr <nlm9380@gmail.com>
2016-10-26 21:19:53 -0500

    Created new interface Filter

--- java-track/assignments/javagram/Javagram.java
+++ java-track/assignments/javagram/Javagram.java
@@ -28,8 +28,11 @@ public class Javagram {
 				System.out.println("Image path (relative to " + dir + "):");
 				relPath = in.next();
 				
-				String[] relPathParts = relPath.split(File.separator);
+				/*String[] relPathParts = relPath.split(File.separator);
 				imagePath = dir + File.separator + String.join(File.separator, Arrays.asList(relPathParts));
+				*/
+				
+				imagePath = (dir + "\\" + relPath);
 				
 				picture = new Picture(imagePath);
 				
@@ -42,8 +45,9 @@ public class Javagram {
 		// TODO - prompt user for filter and validate input
 		
 		// TODO - pass filter ID int to getFilter, and get an instance of Filter back 
-		BlueFilter filter = getFilter();			
-
+		//BlueFilter filter = getFilter();			
+		Filter filter = getFilter();
+		
 		// filter and display image
 		Picture processed = filter.process(picture);
 		processed.show();
@@ -71,9 +75,10 @@ public class Javagram {
 	
 	// TODO - refactor this method to accept an int parameter, and return an instance of the Filter interface
 	// TODO - refactor this method to thrown an exception if the int doesn't correspond to a filter
-	private static BlueFilter getFilter() {
+	private static Filter getFilter() {
 		
 		// TODO - create some more filters, and add logic to return the appropriate one
+		
 		return new BlueFilter();
 		
 	}
